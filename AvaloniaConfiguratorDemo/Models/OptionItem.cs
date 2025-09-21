da7fdb8c5a58b59fb094cde03c7376169938ae15
Minzedefender <djminze@bk.ru>
2025-09-21 22:03:06 +0300

    Delete AvaloniaConfiguratorDemo directory

--- AvaloniaConfiguratorDemo/Models/OptionItem.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace BackupConfiguratorDemo.Models;
-
-public record OptionItem<T>(T Value, string Title)
-{
-    public override string ToString() => Title;
-}
--- AvaloniaConfiguratorDemo/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace BackupConfiguratorDemo.ViewModels;
-
-public class ViewModelBase : ObservableObject
-{
-}
